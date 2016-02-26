4bf4c12705f4926edfe5f739eb420ed5db234842
Jairo Mejia <mail.jairo@gmail.com>
2016-02-26 17:01:52 -0500

    Update default.py

--- config/default.py
+++ config/default.py
@@ -10,21 +10,3 @@ configuración en el archivo `instance/config.py`
 # Flask ++++++++++++++++++++++++++++++++
 DEBUG = False
 # ++++++++++++++++++++++++++++++++++++++
-
-# ++++++++++++++++++++++++++++++++++++++
-GS_AUTH = {
-  'type': 'service_account',
-  'project_id': 'new-api-1203',
-  'private_key_id': 'c94970eb055456a8b64700a9a22e7d0ffef2046c',
-  'private_key': '-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCzmAwcK1NdtAM0\n0t5cOhm0teFX2mj3eFW/q8hFUkHUo3DjRsAlxMdkm/BBqS4KD5aRnGuIgIwBfCep\nitqRX75FPFwohiHdxfuHSXGNMASKN+b2hJ2MYj5gjzyh43dSJIycZAX6OhOYUd7v\n1aov/k5EkmEgWnopHtn8JhsyiYP0WLFjpMNvKeyFWVXbOTfLM2y0BIUZrWvJgTn9\nYs2cnO9F5v3BHq/XBBJBq/Z3GV+FhIqBbgzfzjj4cq5YVpX9k04x9c9vLDEn8CWX\nO2RHymTpDRKaLGDsxqOPpkQ6xWmGpTnAH8kBzYwYmhLD3dYv60BF/qySv+xS/9jq\nhsf6tfelAgMBAAECggEBAKdsj52TJlysfHC6RGFmKKC3DiSAkGpj3EybMFCvpn88\n/vhh9K9BC0FXvlr4BNuMPKnyUK0oaVzZCYCc2bW8s/yHkMKWiOvereYpKZqdsR3J\nj58xck51KAGMhtHE5G/Hcu44sPndOnc9m7+qSMYFJaH3dGRc9JVxFXZjb+dlG4al\nuJu3Shn7bxbEDaXv8mTFOhfrKNPfO75VQzN5gBWerECTRLiGi/R79HUZYIemolN1\nScQ1ldNPKfXFaT9DFD4Cd06V2IPZen7JokwJsRNAxQTaMpTUl7KmqE5lOOHxGY7m\nF90ilTcXLFIzKmalNJlw8/PETnk+yBUjKXXA3vS4AeECgYEA2rd52WD6ClGj02k5\n3SgVEdVisfpE9pL+yVQSlR/Gc+M/ONuuIA2Q/i1CwPV6Tzh4fVsJyW2OlywWrNpx\n46ige0Ttkfr5TDKMUAAdLJKrMeqTvdz8FYb94JWRGhJkDaErdQ2sP6uEBo/qz2zR\nV8N2mVunqTnoYRnWwh4jKR8WjH8CgYEA0jVNczfUmfR5p/JtM3cBqm1PIA623Xk2\nTrOVqUogL9UH88a4gzQuShPiOtT1RX1z+mfLO0NurGdc+2emmdtfizhVEvEwuPHA\nna/flDRtTVFogX5frJyZFRULPtf9FTvthYerEimvlC1GY9kLghVHG52kia0O6j0r\nX9Y8C8ccudsCgYBGFHn3fApCGiybBeFjin9NyoioLF/CfhEjY0vc+mR1J4BPcJxX\nauFlgA0elMlO7vI0gGIfZ3pD1/2f/xJ09sgNF2Xj233bu2qYC6FNceVmxhn2wQMI\nxss8FdB7gR8L+vNYuzvQ3t0b2StMsTpWm6u5sdOD9qkNy+CzLELp3yszcwKBgFX4\nTMOper64OKUSRcz9h+XerLSsr+cmMqq/UZ8Z3M4TLxxpEn8+cQwE08pB3EneQ/eO\nSZTkQDgsUd/UBVP5Xzr9//UuRUHuRVt4WVUwl98+QyJqv9BFf2oa4C2EglIT31F0\nKefUuqU8WwRs+QLrlthZgbh42Ze9Cqc4Y/hhJ4NvAoGBAKdJvhInbC/XwZcSBLSz\nVYFNv9LMkBvJAg8foW0VXbzxP0woHLoMTMzV+9jJwzr+2ODv3hRJyE6fp9AOCNaA\n9dLIEFD/cx9c9Km3gYeeM/lzkBhkxgWpzZ/vyO55LsK46cKVRmUg2ft5VJGXtHV0\nzdeBK+rAtwMnLyf4LeZdVfsj\n-----END PRIVATE KEY-----\n',
-  'client_email': 'desempleo@new-api-1203.iam.gserviceaccount.com',
-  'client_id': '109873841704116900749',
-  'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
-  'token_uri': 'https://accounts.google.com/o/oauth2/token',
-  'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
-  'client_x509_cert_url': 'https://www.googleapis.com/robot/v1/metadata/x509/desempleo%40new-api-1203.iam.gserviceaccount.com'
-}
-
-SPREADSHEET_ID = '17_tkLK1NRJeL1qCMfrqoD0pAJh2NjAuqtKSXnsREFkk'
-# ++++++++++++++++++++++++++++++++++++++
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
