72b0b89a3b5e68809f7e260ce4590744d0f3c0f8
Jairo Mejia <mail.jairo@gmail.com>
2016-02-26 16:47:28 -0500

    Update views.py

--- weekly_oil_report/views.py
+++ weekly_oil_report/views.py
@@ -9,7 +9,7 @@ import gspread
 import nltk.data
 from flask import render_template
 from flask import request
-from oauth2client.client import SignedJwtAssertionCredentials
+##from oauth2client.client import SignedJwtAssertionCredentials
 
 from . import app
 from . import helpers
@@ -19,40 +19,25 @@ from . import helpers
 # Views ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    text = helpers.convert('http://www.dol.gov/ui/data.pdf', pages=[0])
+    text = helpers.convert('http://ir.eia.gov/wpsr/wpsrsummary.pdf', pages=[0])
 
     if request.method == 'GET':
         return render_template('show.html', text=text)
 
-    # Autorizar
-    gc = gspread.authorize(SignedJwtAssertionCredentials(
-        app.config['GS_AUTH']['client_email'],
-        app.config['GS_AUTH']['private_key'].encode(),
-        ['https://spreadsheets.google.com/feeds']
-    ))
-
     # Procesar
     tokenizer = nltk.data.load('nltk:tokenizers/punkt/english.pickle')
-    lineas = tokenizer.tokenize(text)
-    palabras = nltk.word_tokenize(text)
-    cifra1 = u' '.join(palabras[54:58])
-    cifra2 = u' '.join(palabras[49:53]+[', according to data of the Department of Labor'])
-    linea1 = u' '.join(palabras[36:62]).replace(u' ,', u',')
-    linea2 = u' '.join(lineas[1:2])
-    linea3 = u' '.join(lineas[4:5])
-
-    # Obtener datos de desempleo de googlesheet
-    sht1 = gc.open_by_key(app.config['SPREADSHEET_ID'])
-    worksheet = sht1.get_worksheet(0)
-    celda = worksheet.acell('P15').value
-    linea5 = u'The last unemployment figure is {}'.format(celda)
+    lineas=tokenizer.tokenize(text)
+    words = nltk.word_tokenize(text)
+    week= u' '.join(words[7:11])
+    linea1= u' '.join(lineas[8:10]+[', according to government data for the']+week)
+    linea2= u' '.join(lineas[4:6])
+    linea3= u' '.join(lineas[10:11]+lineas[12:13])
+    linea4=u' '.join(lineas[1:2])
 
     return render_template('show.html', **dict(
-        cifra1=cifra1,
-        cifra2=cifra2,
         linea1=linea1,
         linea2=linea2,
         linea3=linea3,
-        linea5=linea5
+        linea4=linea4
     ))
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
