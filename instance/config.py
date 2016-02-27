fabb368d0301f127776e4aa9cbe4209cad3af154
Jairo Mejia <mail.jairo@gmail.com>
2016-02-27 13:03:11 -0500

    Update config.py

--- instance/config.py
+++ instance/config.py
@@ -10,4 +10,24 @@ configuración en el archivo `instance/config.py`
 # Flask ++++++++++++++++++++++++++++++++
 DEBUG = False
 # ++++++++++++++++++++++++++++++++++++++
+# ++++++++++++++++++++++++++++++++++++++
+GS_AUTH = {
+  {
+  "type": "service_account",
+  "project_id": "new-api-1203",
+  "private_key_id": "6c92e16a2383ffb2a75a8f15c6a4576550dc1ae7",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCLUnRfZKQHD0DI\nthbK5Ngu1TNQvf67BrHfFzuHY4D83hKyJn82e31AHq+ZPVXCFRD95irKkB+C0LZB\nqNsCyuRIsJdgKP8YjlgNIH8RrW0NRpNmUgSqzi3uGJwd12OA48sna9iQYcPmFcRu\n/u6ZuMrUqCaHrBq9roAgfNpd+TCut3237ddh0vpVi0S6DaxS9RX5zzOzqL5QrHyR\nQLRDdvqagRArBnASDcowCQLG/1OUIOPtBYZ43kIAnVPSIGge1PUJqf9qAw/bTlii\nxwQhcxQDk9rvKKWVK76b5roVKd8u3xV56owDlDkA3FZBxUj5Ake3hG0hs0L295uc\n5K8SCj3RAgMBAAECggEAfeOJh9DWbhKYRURToA3cUaUP8ztaewB3pM/HWHTOzq6T\nUip69G3tJQ0VOnAzl2WVrvZVYCeOcZPJu9ap3wIZ+Jyoi5k/t56XbXUZnI2KZl6J\nPgGKehRHaEcQed5v6LzsirhmPUA51SOCFZGBY6GYlUNDXYBjwVV2LOTkI9nQ5W1M\nTiBfZq0smUUJloiwCfMSgIoXfr8bFZVpIUmzJD5+mMuPSBY/o5vhgZRsmsVs3WgO\nHdhA6GHHET3KfJQ9iSZ4rueN2xAaJKDeyS1YOBscmnCNrUNxNJK7gv8270Gap6xl\n/bNJGMQzR27d0bMXUmvT6Af6HLR9wYDJ7SiBTLCSCQKBgQDTeOPORL/b2ls3IOK7\nczy1PloqgLLWl/4j/OFwwVwIZa5GAUl0hgZbodcRAj3A4DB1BWTjunTz9bAP5q/e\n1h1tqT3tPI0ejpfT3hVGMxzu04c23V4Arb6cSeelNMrC6fa6IEXCR/3LqCBQnAa7\nXreWFXwjH08gIRdGk9JybRor1wKBgQCoqGn5UBEBAUfIYVAxsvJC0ZaiXBQUm8rk\nX8Hr7J39fhJhfvAo2k2zrsYKkZ+auz/veLb5SacMaAYAtDSnR5w1RtigjIYdM3Qc\nb8XEyifi0GtdvlpHSOtJq2SjU4RSwm9FUdbn0hii0LBPV/plfvTsh72GvqNqD7c/\nrhQjFNxulwKBgHk8YT2r5GJieNDOJa5ZjoBJ0t9zERQ95xEytPwznlOiF5tCuZ34\nCEUYL3gs1MJyxNUR1plIGCyUue8jzcfu8MghssMDZ9QQ47IvK/sm6ntxAZ/OoUm6\nUgC/zfHEpknAYBas0dFj/Hh5Vq1gBev9jTqZ7qUkGfTNg86OiKPZMJvZAoGAJcQI\nbtY56IKz4y+/OFG8o6+wzkxWBsNX7Elw7/Y87m1TildVSaCQc6K4H9e56HjzeRkL\nx8hxSnrklTXE8Iw9c0SY72JoCZPi9RcmLsmhos1OrWAZcQrn/MgPMe9amvmE1v3b\nZG+MKrEWOb4WiLGkAG7oxopbpz07qFu/kJd0QKMCgYBMmywhe90mZSb9qrCbP8YP\np0h1CgILBtwSuJXDoL89eVubDcs8YWQfcxhxkmIigh2mw+eQtrOiN4XD+TxfSE2g\n0xofBKJUWiTQfb4lDvZmWLbwAKBNcMRv4n3z3TF1/KXLPKm9VSJ2tpjmvMWfXGq0\n9Kuvmz+C1SNUbMXpCpyeAg==\n-----END PRIVATE KEY-----\n",
+  "client_email": "new-api-1203@appspot.gserviceaccount.com",
+  "client_id": "117547075419073672897",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://accounts.google.com/o/oauth2/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/new-api-1203%40appspot.gserviceaccount.com"
+}
+
+}
+
+SPREADSHEET_ID = '1XwtSVnvikI3EzBKVoPfFsxMCaH0H-Un54DUiJc2CBK4'
+# ++++++++++++++++++++++++++++++++++++++
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
