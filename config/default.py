958a08f14c7aa054863a5bf6347b5b3f35ff2c44
Jairo Mejia <mail.jairo@gmail.com>
2016-02-27 13:25:26 -0500

    Update default.py

--- config/default.py
+++ config/default.py
@@ -13,16 +13,17 @@ DEBUG = False
 # ++++++++++++++++++++++++++++++++++++++
 GS_AUTH = {
   "type": "service_account",
-  "project_id": "new-api-1203",
-  "private_key_id": "6c92e16a2383ffb2a75a8f15c6a4576550dc1ae7",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCLUnRfZKQHD0DI\nthbK5Ngu1TNQvf67BrHfFzuHY4D83hKyJn82e31AHq+ZPVXCFRD95irKkB+C0LZB\nqNsCyuRIsJdgKP8YjlgNIH8RrW0NRpNmUgSqzi3uGJwd12OA48sna9iQYcPmFcRu\n/u6ZuMrUqCaHrBq9roAgfNpd+TCut3237ddh0vpVi0S6DaxS9RX5zzOzqL5QrHyR\nQLRDdvqagRArBnASDcowCQLG/1OUIOPtBYZ43kIAnVPSIGge1PUJqf9qAw/bTlii\nxwQhcxQDk9rvKKWVK76b5roVKd8u3xV56owDlDkA3FZBxUj5Ake3hG0hs0L295uc\n5K8SCj3RAgMBAAECggEAfeOJh9DWbhKYRURToA3cUaUP8ztaewB3pM/HWHTOzq6T\nUip69G3tJQ0VOnAzl2WVrvZVYCeOcZPJu9ap3wIZ+Jyoi5k/t56XbXUZnI2KZl6J\nPgGKehRHaEcQed5v6LzsirhmPUA51SOCFZGBY6GYlUNDXYBjwVV2LOTkI9nQ5W1M\nTiBfZq0smUUJloiwCfMSgIoXfr8bFZVpIUmzJD5+mMuPSBY/o5vhgZRsmsVs3WgO\nHdhA6GHHET3KfJQ9iSZ4rueN2xAaJKDeyS1YOBscmnCNrUNxNJK7gv8270Gap6xl\n/bNJGMQzR27d0bMXUmvT6Af6HLR9wYDJ7SiBTLCSCQKBgQDTeOPORL/b2ls3IOK7\nczy1PloqgLLWl/4j/OFwwVwIZa5GAUl0hgZbodcRAj3A4DB1BWTjunTz9bAP5q/e\n1h1tqT3tPI0ejpfT3hVGMxzu04c23V4Arb6cSeelNMrC6fa6IEXCR/3LqCBQnAa7\nXreWFXwjH08gIRdGk9JybRor1wKBgQCoqGn5UBEBAUfIYVAxsvJC0ZaiXBQUm8rk\nX8Hr7J39fhJhfvAo2k2zrsYKkZ+auz/veLb5SacMaAYAtDSnR5w1RtigjIYdM3Qc\nb8XEyifi0GtdvlpHSOtJq2SjU4RSwm9FUdbn0hii0LBPV/plfvTsh72GvqNqD7c/\nrhQjFNxulwKBgHk8YT2r5GJieNDOJa5ZjoBJ0t9zERQ95xEytPwznlOiF5tCuZ34\nCEUYL3gs1MJyxNUR1plIGCyUue8jzcfu8MghssMDZ9QQ47IvK/sm6ntxAZ/OoUm6\nUgC/zfHEpknAYBas0dFj/Hh5Vq1gBev9jTqZ7qUkGfTNg86OiKPZMJvZAoGAJcQI\nbtY56IKz4y+/OFG8o6+wzkxWBsNX7Elw7/Y87m1TildVSaCQc6K4H9e56HjzeRkL\nx8hxSnrklTXE8Iw9c0SY72JoCZPi9RcmLsmhos1OrWAZcQrn/MgPMe9amvmE1v3b\nZG+MKrEWOb4WiLGkAG7oxopbpz07qFu/kJd0QKMCgYBMmywhe90mZSb9qrCbP8YP\np0h1CgILBtwSuJXDoL89eVubDcs8YWQfcxhxkmIigh2mw+eQtrOiN4XD+TxfSE2g\n0xofBKJUWiTQfb4lDvZmWLbwAKBNcMRv4n3z3TF1/KXLPKm9VSJ2tpjmvMWfXGq0\n9Kuvmz+C1SNUbMXpCpyeAg==\n-----END PRIVATE KEY-----\n",
-  "client_email": "new-api-1203@appspot.gserviceaccount.com",
-  "client_id": "117547075419073672897",
+  "project_id": "project-id-hkyjukdnhqhentrycje",
+  "private_key_id": "968821e8c22a7ea77365ae6cb6b93235090466a0",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCNzwTokdteDPKv\nBWY9CnjPB8Ny0Ip0GqhUOV9Bm5Abeiiu9MDTkBjV/z1SWlOma5450nErHpkYfVKo\nHCtKKYxAitcZ0SgiKPXQUYaJlmg7dT03KmWAug83g47T1+/VzPOGm82xkhIvqWYb\ndh9FwF9ap648yRIsgfsRqf3it4mMlQRNf6ts5oUJMz3tCWfx+E0qC4GomQS0VcQ6\nj/y1E07Rn5nz9xi1Yy849iumof4p9Ki0Z1Yed6nrbzM3lVBd6j1tmVW9K8bUiCPs\n5V8J4j/yCuY2g6loXcEcrcuNlSTnnDezZX/WHtP5++mswi4RbyZeIQCvS6hw1WpR\n3wDGjV9NAgMBAAECggEAUjRq7QL4fqnU2P6xSLtUUvd2Jo2bZTi60bA8dwTd4f/L\nq7g4O6wxqYP/oUi5ayTBoVVLwypGp3qmussxkRlKs3OIw++Ka1sBPFS2P/YP0D/j\nT6JlGt1AyvaZilaIMpFPrn2NHN3HaxexGEQ+Vqnw3NWNXLFWtrRWveOkSJQKyZIO\n97t6Vb+xBtnJq77RETfQlZOlq6Uv8ObaXXOy4ORP4TjYnT2tUB7fB8JZCCQjRpGQ\naMpRIY+YOzTAGhYQRvFlu0ch1x+45dKTrwXR85qN/OaeT4IA8Qcj6KhiXMlAx7O1\nQ2abBgCP7PdD5pY8HdObPx0/1lksfSHA9VDLN+7p4QKBgQDRkM4QSpyvjOdBjekp\n0FLdnkG5dBGfFVhah/qRdSgvul5U+FSfCuUlAbHB/299BU/JfoQVaN5KgjLPVNI9\n/A6NMlvYxgOYSaMVoVRDB5lKY81apkGyOB3Z74VdA8Hkx7U3E3QypQHPbb0zOFxm\nHXlDKJ8jkzUwKN+L1IjNJ+3GpQKBgQCtOtWWu3I5w2inhIAzBrhg+ZN2OulBXh7O\niUiJslsmRvOTjXPXbsZqnJqjoJllyXzOKOy/bTCQfsWmbBcauyvukiWQ3nvW4Ohf\n3d5P1pAGiI5KWtOUh+Hos12M/rZ1xr/hGgRa358Y2OFD0/6xE1fgndlBPSc5AwgD\n3gPHu3n9iQKBgHd3BQFkh03MFUjZjUG48IRF41HFSQZXNLbnLYAeOXp706q8pgDE\nRTyDasIcy4GJtaI37TQHRDFVZ4Gp6PBM8D6tpo4802/NCpsaDN7pRgmSO06h2HGw\na9QBJxWPPskcz8FF5ce5KP8GoATC7GmKA8a1MYMmN/vsUQN1dUb+rB3BAoGAK+AF\n7wqE8NpsbFlj+wiNtIoh7kUXhSXomZkt1Spmabh4wvuFxp/ZA3GsvRrVH3G7+Z9l\n70ohjcd+GhuwcmXLx74eY6XmKdOHJ5jTUNdgXTdDCtQcWLdd4YlkfWyaHBR9zu6/\nAaE385wi2gIctQ7YKBzyxNJPV+JLqmcWAAWc7TkCgYEAxIj3dpoC77IwXQiOug1n\n3tHxur1Fvd10soy0t9KU4A+w1BELubhk0iWwGgVi+gWQuEgo+X99SV+0jCq9KKJG\nI210E+tRwzs5vjsPQaByh41Nu9PXLGh3dJQEdrJoCYOL69d5dH0DSoHMCuqKWLU/\n9g3L+RubCSNyojwCmNsfzEs=\n-----END PRIVATE KEY-----\n",
+  "client_email": "weeklyoilreport@project-id-hkyjukdnhqhentrycje.iam.gserviceaccount.com",
+  "client_id": "113579982394054010452",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://accounts.google.com/o/oauth2/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/new-api-1203%40appspot.gserviceaccount.com"
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/weeklyoilreport%40project-id-hkyjukdnhqhentrycje.iam.gserviceaccount.com"
 }
+
 
 SPREADSHEET_ID = '1XwtSVnvikI3EzBKVoPfFsxMCaH0H-Un54DUiJc2CBK4'
 # ++++++++++++++++++++++++++++++++++++++
